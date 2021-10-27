fe2f7c8ac336b23ea716464cea498cf24109588a
Raul <ravels1991@gmail.com>
2021-10-26 21:59:53 -0300

    Meu segundo post

---
+++ content/post/Livros.md
@@ -0,0 +1,9 @@
+---
+title: "Livros 📚"
+date: 2021-10-26T21:57:26-03:00
+pin: true
+---
+
+
+Livros 📚
+
---
+++ deploy.sh
@@ -0,0 +1,14 @@
+
+hugo
+commit= "no message for this commit"
+[[ "${1}" ]] && commit=${1}
+
+cd public
+git add .
+git commit -m "${commit}"
+git push origin main
+
+cd ..
+git add .
+git commit -m "${commit}"
+git push origin main
